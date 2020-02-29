4ffdb9b3eb3a9dcb68f02269a75be6d5b5d68639
sidekick9497 <iharish@protonmail.com>
2020-02-28 20:26:19 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AppointmentComponent,
+    ViewAppointmentComponent
   ],
   imports: [
     BrowserModule
